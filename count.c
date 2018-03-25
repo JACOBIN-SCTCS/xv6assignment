5b69eadf93295e74c2e824c6181c95308e213d80
jacob <jacobja3@gmail.com>
2018-03-25 14:53:53 +0530

    First Commit

---
+++ count.c
@@ -0,0 +1,16 @@
+#include "types.h"
+#include "stat.h"
+
+#include "user.h"
+
+
+int 
+main(void)
+{
+   printf(1, "TOTAL NUMBER OF CALLS MADE SO FAR = %d \n" ,getcount());
+   exit();
+}
+
+
+
+   
